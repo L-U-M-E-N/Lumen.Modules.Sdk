7ca7bb76a4ba10411299c7a573a3626725973113
Elanis <elanis@hotmail.com>
2024-12-23 17:30:01 +0100

    SDK Base

---
+++ Lumen.Modules.Sdk/LumenModuleBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Lumen.Modules.Sdk {
+    public abstract class LumenModuleBase {
+        private readonly LumenModuleRunsOnFlag runsOn;
+        protected readonly ILogger<LumenModuleBase> logger;
+
+        protected LumenModuleBase(LumenModuleRunsOnFlag runsOn, ILogger<LumenModuleBase> logger) {
+            this.runsOn = runsOn;
+            this.logger = logger;
+        }
+
+        public bool IsAPIModule() {
+            return runsOn.HasFlag(LumenModuleRunsOnFlag.API);
+        }
+
+        public bool IsUIModule() {
+            return runsOn.HasFlag(LumenModuleRunsOnFlag.UI);
+        }
+
+        public bool IsWorkerModule() {
+            return runsOn.HasFlag(LumenModuleRunsOnFlag.Worker);
+        }
+
+        public abstract Task InitAsync(LumenModuleRunsOnFlag currentEnv);
+        public abstract Task ShutdownAsync();
+        public abstract Task RunAsync(LumenModuleRunsOnFlag currentEnv);
+        public abstract bool ShouldRunNow(LumenModuleRunsOnFlag currentEnv);
+    }
+}
---
+++ Lumen.Modules.Sdk/LumenModuleRunsOnFlag.cs
@@ -0,0 +1,9 @@
+﻿namespace Lumen.Modules.Sdk {
+    [Flags]
+    public enum LumenModuleRunsOnFlag {
+        None = 0,
+        UI = 1,
+        Worker = 2,
+        API = 3
+    }
+}
