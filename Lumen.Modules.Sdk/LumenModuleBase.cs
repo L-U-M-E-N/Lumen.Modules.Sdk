a6b864b2b3a49992de698de78c16673e739cb5e9
Elanis <elanis@hotmail.com>
2024-12-26 10:53:06 +0100

    Add database-related methods to implement on LumenModuleBase
    This is concieved in a way you don't need to install Efcore as a dependency if you're not using database
    Tbd if we change that later

--- Lumen.Modules.Sdk/LumenModuleBase.cs
+++ Lumen.Modules.Sdk/LumenModuleBase.cs
@@ -10,21 +10,28 @@ namespace Lumen.Modules.Sdk {
             this.logger = logger;
         }
 
+        // Generic methods
         public bool IsAPIModule() {
             return runsOn.HasFlag(LumenModuleRunsOnFlag.API);
         }
-
         public bool IsUIModule() {
             return runsOn.HasFlag(LumenModuleRunsOnFlag.UI);
         }
-
         public bool IsWorkerModule() {
             return runsOn.HasFlag(LumenModuleRunsOnFlag.Worker);
         }
 
+        // Standard event loop methods
         public abstract Task InitAsync(LumenModuleRunsOnFlag currentEnv);
         public abstract Task ShutdownAsync();
         public abstract Task RunAsync(LumenModuleRunsOnFlag currentEnv);
+
+        // Scheduled events
         public abstract bool ShouldRunNow(LumenModuleRunsOnFlag currentEnv);
+
+        // Database-related methods
+        public abstract bool IsUsingDatabase(LumenModuleRunsOnFlag currentEnv);
+        public abstract Type GetModuleDatabaseContext();
+        public abstract string GetModuleDatabaseContextSchemaName();
     }
 }
